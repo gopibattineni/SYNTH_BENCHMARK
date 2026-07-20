1eb7bba4bd7a2e7d163b21f8800b01b54ce1e32b
gopibattineni <gopi.chandu89@gmail.com>
2026-07-20 12:15:01 +0100

    Add Adult MMD scores for SDV generators to the dashboard.

--- docs/app.js
+++ docs/app.js
@@ -451,7 +451,7 @@ function axisSpec(metric, values, { clampUnit = false } = {}) {
   };
 }
 
-const DATA_VERSION = "20260720c";
+const DATA_VERSION = "20260720d";
 
 async function loadJSON(name) {
   const url = `data/${name}?v=${DATA_VERSION}`;
